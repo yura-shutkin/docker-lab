372bdeb22ca4e3d73d2dca732390530b58995f63
Shutkin Yura <shutkin.yurii@gmail.com>
2018-09-04 13:27:07 +0500

    Added vault-client configs and build

---
+++ vault-cluster/Client-srv/client-srv-startup.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env sh
+
+consul agent -config-dir /consul/config/ &
+
+COUNTDOWN=30
+COUNTER=0
+while [ $COUNTER -le $COUNTDOWN ]
+do
+  if [ $COUNTER -eq $COUNTDOWN ]; then
+    echo "Consul did not start in $COUNTDOWN sec"
+    exit 1
+  fi
+  nc -zv 127.0.0.1 8500
+  if [ $? -ne 0 ]; then
+    echo "sleeping 1 sec"
+    sleep 1
+    COUNTER=$((COUNTER+1))
+  else
+    break
+  fi
+done
+
+vault server --config=/vault/config/config.hcl
+
+#COUNTDOWN=30
+#COUNTER=0
+#while [ $COUNTER -le $COUNTDOWN ]
+#do
+#  if [ $COUNTER -eq $COUNTDOWN ]; then
+#    echo "Vault did not start in $COUNTDOWN sec"
+#    exit 1
+#  fi
+#  nc -zv 127.0.0.1 8200
+#  if [ $? -ne 0 ]; then
+#    echo "sleeping 1 sec"
+#    sleep 1
+#    COUNTER=$((COUNTER+1))
+#  else
+#    break
+#  fi
+#done
---
+++ vault-cluster/configs/vault-config-node-3.hcl
@@ -0,0 +1,17 @@
+storage "consul" {
+  address = "127.0.0.1:8500"
+  path = "vault/"
+  scheme = "http"
+  token = "d565f7b1-a1ac-f91a-eaa5-820ca3f03124"
+}
+
+listener "tcp" {
+  address     = "192.168.4.8:8200"
+  tls_disable = "true"
+  cluster_address = "192.168.4.8:8201"
+}
+
+ui = "true"
+
+# api_addr = "http://192.168.4.5:8200"
+# cluster_addr = "https://192.168.4.5:8200"
