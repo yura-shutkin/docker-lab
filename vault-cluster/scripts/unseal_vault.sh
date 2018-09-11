85d401e67a4212054b16db2304ac490202d9bcfe
Shutkin Yura <shutkin.yurii@gmail.com>
2018-09-11 23:43:12 +0500

    Added vault-unseal script

---
+++ vault-cluster/scripts/unseal_vault.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+VAULT_SERVERS="192.168.4.6 192.168.4.7 192.168.4.8"
+VAULT_PORT="8200"
+VAULT_KEY=$(cat ./vault-key)
+
+for vault_srv in $VAULT_SERVERS; do
+  curl -X PUT --data "{ "\""key"\"": "\""$VAULT_KEY"\"" }" http://$vault_srv:$VAULT_PORT/v1/sys/unseal
+done
