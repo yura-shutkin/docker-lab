c171e11680820073671f89d7d086e8672c7fb652
Shutkin Yura <shutkin.yurii@gmail.com>
2018-11-16 20:27:03 +0500

    Remove unnecessary variable and add double quote to script variable

--- vault-cluster/scripts/unseal_vault.sh
+++ vault-cluster/scripts/unseal_vault.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
 VAULT_SERVERS="vault-client-1 vault-client-2 vault-client-3"
-VAULT_PORT="443"
 VAULT_KEY=$(cat ./vault-key)
 
 for vault_srv in $VAULT_SERVERS; do
-  docker exec -ti $vault_srv vault operator unseal -address=http://127.0.0.1:8200 "$VAULT_KEY"
+  docker exec -ti "$vault_srv" vault operator unseal -address=http://127.0.0.1:8200 "$VAULT_KEY"
 done
